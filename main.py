62347a1e328813ff0eb1d702abb6111124f3d9e9
Jordan Lee <jorlee92@gmail.com>
2021-02-15 23:30:06 -0700

    Adding files so that I can submit

---
+++ main.py
@@ -0,0 +1,26 @@
+import urllib.request
+import json
+apiKey = "L1I1Y71YJ5CIIBHR"
+
+
+def getStockData(stockName):
+    url = "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + stockName + "&apikey=" + apiKey
+    connection = urllib.request.urlopen(url)
+    result = connection.read().decode()
+    print(result)
+    return json.loads(result)
+
+
+def main():
+    while True:
+        inp = input("ENTER A STOCK NAME : ")
+        if(inp == "exit"):
+            break
+        result = getStockData(inp)
+        toPrint = "The current price of " + inp + " is " + str(result['Global Quote']['05. price'])
+        print(toPrint)
+
+
+if __name__ == '__main__':
+    main()
+
